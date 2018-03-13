d3b20060c8897d38108f9a3ede876363512c040a
Squibs <z.r.holman+github@gmail.com>
2018-03-13 15:23:58 -0600

    Multiple html pages supported.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,25 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const fs = require('fs'); // node file system module (to read directory contents)
+
+// thank you to Frank Kelleher (extri.co)
+// https://extri.co/2017/07/11/generating-multiple-html-pages-with-htmlwebpackplugin/
+function generateHtmlPlugins(templateDir) {
+  // retrieve directory contents of passed directory
+  let templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
+
+  // filter out any non-html directory contents
+  templateFiles = templateFiles.filter(item => /\.html$/i.test(item));
+
+  // return new array created from each html item
+  return templateFiles.map(item => new HtmlWebPackPlugin({
+    filename: item,
+    template: path.resolve(__dirname, `${templateDir}/${item}`),
+    favicon: './src/favicon/favicon.ico',
+  }));
+}
+
+const htmlPlugins = generateHtmlPlugins('./src/');
 
 module.exports = {
   entry: './src/entry.js',
@@ -30,12 +50,7 @@ module.exports = {
   },
 
   plugins: [
-    new HtmlWebPackPlugin({
-      template: './src/index.html',
-      filename: './index.html',
-      favicon: './src/favicon/favicon.ico',
-    }),
-  ],
+  ].concat(htmlPlugins),
 
   devServer: {
     contentBase: './build',
