8cea581c059dee812aff6fbb23cddf1ee2325440
Squibs <z.r.holman+github@gmail.com>
2018-03-14 16:41:46 -0600

    Added slight bit of content to html pages.

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,12 +1,15 @@
 // FontAwesome imports (https://fontawesome.com/how-to-use/use-with-node-js#free)
 import fontawesome from '@fortawesome/fontawesome';
 import faFontAwesomeAlt from '@fortawesome/fontawesome-free-brands/faFontAwesomeAlt';
+import faGulp from '@fortawesome/fontawesome-free-brands/faGulp';
+import faGrunt from '@fortawesome/fontawesome-free-brands/faGrunt';
+import faGithubAlt from '@fortawesome/fontawesome-free-brands/faGithubAlt';
 
 import 'bootstrap'; // auto imports jQuery and Popper for bootstrap
 import './main';
 
 // add each icon to fontawesome library
-fontawesome.library.add(faFontAwesomeAlt);
+fontawesome.library.add(faFontAwesomeAlt, faGulp, faGrunt, faGithubAlt);
 
 // //////////////////////////////////////////////////////////////////////////////////
 // Reduce entry.js size by importing Bootstrap plugins individually
@@ -18,7 +21,7 @@ fontawesome.library.add(faFontAwesomeAlt);
 // ////////////////////////////////////////////////////////////////////////
 
 // Show proof of entry code
-console.log("I'm just a silly little entry point!");
+console.log('I\'m just a silly little entry point!');
 
 // sample ES6 code will be transpiled by Babel then minimized on build
 const app = (a, b) => a + b;
